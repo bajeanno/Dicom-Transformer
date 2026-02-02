8ee873911544b8836ec1833b5d78f78bf914bf22
bolide <bajeanno@student.42lyon.fr>
2026-02-02 10:25:08 +0100

    refactor: removed logging system as it won't work

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,18 @@ use axum::Router;
 use axum::routing::{get, post};
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
-use tower_http::{
-    services::{ServeDir, ServeFile},
-    trace::TraceLayer,
-};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-
+use tower_http::services::{ServeDir, ServeFile};
 async fn route() -> &'static str {
     "test"
 }
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| "info".into()),
-        ))
-        .with(tracing_subscriber::fmt::layer())
-        .init();
     let addr: SocketAddr = "0.0.0.0:8080".parse().unwrap();
 
-    let app = Router::new()
-        .route("/path", get(route))
-        .fallback_service(
-            ServeDir::new("frontend/dist").fallback(ServeFile::new("frontend/dist/index.html")),
-        )
-        .layer(TraceLayer::new_for_http());
+    let app = Router::new().route("/path", get(route)).fallback_service(
+        ServeDir::new("frontend/dist").fallback(ServeFile::new("frontend/dist/index.html")),
+    );
 
     axum::serve(
         TcpListener::bind(addr).await.unwrap(),
